48ad21b27102db3ac6456c4c645070e265230032
penguin311 <lst5561@naver.com>
2023-04-15 11:21:20 +0900

    end_lst

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -1,93 +1,86 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Row, Col, Button, Form } from "react-bootstrap";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import {Row, Col, Button, Form, InputGroup, Card} from 'react-bootstrap'
+import {BsSearch} from 'react-icons/bs'
+import axios from 'axios'
+import Book from './Book';
 
 const BookPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [books, setBooks] = useState([]);
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(0);
-  const [is_end, setIs_end] = useState(false);
-  const [query, setQuery] = useState("리액트");
-  const ref_query = useRef(null);
+    const [loading, setLoading] = useState(false);
+    const [books, setBooks] = useState([]);
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(0);
+    const [is_end, setIs_end] = useState(false);
+    const [query, setQuery] = useState('리액트');
 
-  const onSummit = (e) => {
-    e.preventDefault();
-    getBooks();
-  };
+    const getBooks = async() => {
+        const url = "https://dapi.kakao.com/v3/search/book?target=title";
+        const config = {
+            headers: {"Authorization": "KakaoAK b80880fbde422de3fd9b4a4e67c9bb54"},
+            params: {"query": query, "size": 8, "page": page}
+        }
+        setLoading(true);
+        const result = await axios(url, config);
+        console.log(result.data);
+        const data=result.data.documents.map(book=>{
+            const fmtPrice=book.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            return { ...book, fmtPrice:fmtPrice, show:false }
+        })
+        setBooks(data);
+        setTotal(result.data.meta.pageable_count);
+        setIs_end(result.data.meta.is_end);
+        setLoading(false);
+    }
 
-  const getBooks = async () => {
-    const url = "https://dapi.kakao.com/v3/search/book?target=title";
-    const config = {
-      headers: { Authorization: "KakaoAK 4361e376f3dd2e9b4ec4f09da086421b" },
-      params: { query: query, size: 6, page: page },
-    };
+    useEffect(()=>{
+        getBooks();
+    },[page]);
 
-    setLoading(true);
-    const result = await axios(url, config);
+    const onSearch = () => {
+        setPage(1);
+        getBooks();
+    }
 
-    setTotal(result.data.meta.pageable_count);
-    setIs_end(result.data.meta.is_end);
-    setBooks(result.data.documents);
-    console.log(result);
-    setLoading(false);
-    ref_query.current.focus();
-  };
-
-  useEffect(() => {
-    getBooks();
-  }, [page]);
-
-  if (loading) return <h1 className="text-center my-5">로딩중임 ...</h1>;
-  return (
-    <Row className="my-5 mx-2">
-      <Row>
-        <Col>
-          <Form onSubmit={onSummit}>
-            <Form.Control
-              onChange={(e) => setQuery(e.target.value)}
-              value={query}
-              placeholder="검색어"
-              ref={ref_query}
-            />
-          </Form>
-        </Col>
-        <Col>검색수 : {total}</Col>
-      </Row>
-      <Col>
-        <h1 className="text-center">도서검색</h1>
-        <Row>
-          {books.map((book) => (
-            <Col key={book.isbn} className="box my-2">
-              <div>
-                <img
-                  src={
-                    !book.thumbnail
-                      ? "http://via.placeholder.com/120x170"
-                      : book.thumbnail
-                  }
-                />
-              </div>
-              <div className="eilipsis">{book.title}</div>
-              <div className="eilipsis">{book.price}원</div>
-            </Col>
-          ))}
+    if(loading) return <h1 className='text-center my-5'>로딩중......</h1>
+    return (
+        <Row className='justify-content-center mx-2 my-5'>
+            <h1 className='text-center mb-5'>도서검색</h1>
+            <Row className='my-2'>
+                <Col md={4} xs={6}>
+                    <Form onSubmit={ onSearch }>
+                        <InputGroup>
+                            <Form.Control value={query} placeholder='검색어'
+                                onChange={(e)=>setQuery(e.target.value)}/>
+                            <InputGroup.Text><BsSearch/></InputGroup.Text>
+                        </InputGroup>
+                    </Form>
+                </Col>
+                <Col>검색수: {total}건</Col>
+            </Row>
+            <hr/>
+            <Row className='my-2 justify-content-center'>
+                {books.map(book=>
+                    <Col key={book.isbn} className="my-2" md={3} xs={6}>
+                        <Card>
+                            <Card.Body>
+                                <div><img src={!book.thumbnail ? 'http://via.placeholder.com/120x170':book.thumbnail}/></div>
+                                <div className="ellipsis">{book.title}</div>
+                                <div>{book.fmtPrice}원</div>
+                                <div className="ellipsis">{book.authors}</div>
+                                <Book book={book}/>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )}
+            </Row>
+            <div className='text-center my-3'>
+                <Button disabled={page === 1 && true}
+                    onClick={()=>setPage(page-1)}>이전</Button>
+                <span className='mx-2'>{page}</span>
+                <Button disabled={is_end}
+                    onClick={()=>setPage(page+1)}>다음</Button>
+            </div>
         </Row>
-      </Col>
-      <div>
-        <Button disabled={page == 1 && true} onClick={() => setPage(page - 1)}>
-          이전
-        </Button>
-        <span className="mx-3">{page}</span>
-        <Button
-          disabled={is_end == 1 && true}
-          onClick={() => setPage(page + 1)}
-        >
-          다음
-        </Button>
-      </div>
-    </Row>
-  );
-};
+    )
+}
 
-export default BookPage;
+export default BookPage
--- src/components/RouterPage.jsx
+++ src/components/RouterPage.jsx
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link, Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import BookPage from './BookPage';
+import LocalPage from './LocalPage';
 
 const RouterPage = () => {
     return (
@@ -14,8 +15,9 @@ const RouterPage = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Link to="/">홈</Link>
+                            <Link to="/">Home</Link>
                             <Link to="/books">도서검색</Link>
+                            <Link to="/locals">지역검색</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -23,6 +25,7 @@ const RouterPage = () => {
             <Switch>
                 <Route path="/" component={HomePage} exact={true}/>
                 <Route path="/books" component={BookPage}/>
+                <Route path="/locals" component={LocalPage}/>
             </Switch>
         </>
     )
